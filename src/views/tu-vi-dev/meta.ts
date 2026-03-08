a2ae87c67d91397f0ac14f68029a399b9e76909c
HungDoan <hungdoan.it95@gmail.com>
2026-03-08 10:42:24 +0700

    Update metadata for the tu-vi-dev view.

--- src/views/tu-vi-dev/meta.ts
+++ src/views/tu-vi-dev/meta.ts
@@ -4,7 +4,7 @@ const meta: PageMeta = {
     name: 'Tử Vi Dev',
     description: 'Xem lá số tử vi lập trình viên — phân tích code để tìm cung dev của bạn 🌌',
     author: 'hwg',
-    facebook: 'https://www.facebook.com/groups/j2team.community',
+    facebook: 'https://khonggianai.com/',
 }
 
 export default meta
